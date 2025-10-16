2d185e9d8d64dbb9af454c9b005b9a6bba0e616e
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-10-16 10:03:02 +0000

    Split App and State into separate files

--- src/web/renderer.rs
+++ src/web/renderer.rs
@@ -1,36 +1,21 @@
 mod node_types;
 mod vertex_buffer;
 
-use glam::Vec2;
-use log::info;
-use specs::WorldExt;
-use std::sync::Arc;
-use wgpu::util::DeviceExt;
-#[cfg(target_arch = "wasm32")]
-use winit::platform::web::EventLoopExtWebSys;
-use winit::{
-    application::ApplicationHandler,
-    event::*,
-    event_loop::{ActiveEventLoop, EventLoop},
-    keyboard::{KeyCode, PhysicalKey},
-    window::Window,
-};
-
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
-
-use vertex_buffer::{NodeInstance, VERTICES, Vertex};
-
-use crate::web::simulator::{Simulator, components::nodes::Position};
-
 use crate::web::renderer::node_types::NodeType;
-
+use crate::web::simulator::{Simulator, components::nodes::Position};
+use glam::Vec2;
 use glyphon::{
     Attrs, Buffer as GlyphBuffer, Cache, Color, Family, FontSystem, Metrics, Resolution, Shaping,
     SwashCache, TextArea, TextAtlas, TextBounds, TextRenderer, Viewport,
 };
+use log::info;
+use specs::WorldExt;
+use std::sync::Arc;
+use vertex_buffer::{NodeInstance, VERTICES, Vertex};
+use wgpu::util::DeviceExt;
+use winit::{event_loop::ActiveEventLoop, keyboard::KeyCode, window::Window};
 
-struct State {
+pub struct State {
     surface: wgpu::Surface<'static>,
     device: wgpu::Device,
     queue: wgpu::Queue,
@@ -66,7 +51,7 @@ struct State {
     text_renderer: Option<TextRenderer>,
     // one glyphon buffer per node containing its text (created when glyphon is initialized)
     text_buffers: Option<Vec<GlyphBuffer>>,
-    window: Arc<Window>,
+    pub window: Arc<Window>,
 }
 
 impl State {
@@ -168,7 +153,7 @@ impl State {
         });
 
         // TODO: remove test code after adding simulator
-        let positions = [[50.0, 50.0], [250.0, 250.0], [450.0, 450.0]];
+        let positions = [[50.0, 25.0], [250.0, 50.0], [50.0, 450.0]];
         let labels = vec![
             String::from("My class"),
             String::from("Loooooooong class"),
@@ -606,9 +591,11 @@ impl State {
         Ok(())
     }
 
-    fn update(&mut self) {
-        self.simulator.dispatcher.dispatch(&self.simulator.world);
-        self.simulator.world.maintain();
+    pub fn update(&mut self) {
+        if !self.paused {
+            self.simulator.dispatcher.dispatch(&self.simulator.world);
+            self.simulator.world.maintain();
+        }
 
         let positions_storage = self.simulator.world.read_storage::<Position>();
         for (i, entity) in self.simulator.entities.iter().enumerate() {
@@ -648,7 +635,7 @@ impl State {
             .write_buffer(&self.node_instance_buffer, 0, bytemuck::cast_slice(&nodes));
     }
 
-    fn handle_key(&mut self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+    pub fn handle_key(&mut self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
         match (code, is_pressed) {
             (KeyCode::Escape, true) => event_loop.exit(),
             (KeyCode::Space, true) => {
@@ -658,159 +645,4 @@ impl State {
             _ => {}
         }
     }
-}
-
-struct App {
-    #[cfg(target_arch = "wasm32")]
-    proxy: Option<winit::event_loop::EventLoopProxy<State>>,
-    state: Option<State>,
-}
-
-impl App {
-    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
-        #[cfg(target_arch = "wasm32")]
-        let proxy = Some(event_loop.create_proxy());
-        Self {
-            state: None,
-            #[cfg(target_arch = "wasm32")]
-            proxy,
-        }
-    }
-}
-
-impl ApplicationHandler<State> for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        #[allow(unused_mut)]
-        let mut window_attributes = Window::default_attributes();
-
-        #[cfg(target_arch = "wasm32")]
-        {
-            use wasm_bindgen::JsCast;
-            use web_sys::wasm_bindgen::UnwrapThrowExt;
-            use winit::platform::web::WindowAttributesExtWebSys;
-
-            const CANVAS_ID: &str = "canvas";
-
-            let window = web_sys::window().unwrap_throw();
-            let document = window.document().unwrap_throw();
-            let canvas = document.get_element_by_id(CANVAS_ID).unwrap_throw();
-            let html_canvas_element = canvas.unchecked_into();
-            window_attributes = window_attributes.with_canvas(Some(html_canvas_element));
-        }
-
-        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
-
-        #[cfg(target_arch = "wasm32")]
-        {
-            // Run the future asynchronously and use the
-            // proxy to send the results to the event loop
-            if let Some(proxy) = self.proxy.take() {
-                wasm_bindgen_futures::spawn_local(async move {
-                    assert!(
-                        proxy
-                            .send_event(
-                                State::new(window)
-                                    .await
-                                    .expect("Unable to create canvas!!!")
-                            )
-                            .is_ok()
-                    )
-                });
-            }
-        }
-    }
-
-    #[allow(unused_mut)]
-    fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
-        // This is where proxy.send_event() ends up
-        #[cfg(target_arch = "wasm32")]
-        {
-            event.window.request_redraw();
-            event.resize(
-                event.window.inner_size().width,
-                event.window.inner_size().height,
-            );
-        }
-        self.state = Some(event);
-    }
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        _window_id: winit::window::WindowId,
-        event: WindowEvent,
-    ) {
-        let state = match &mut self.state {
-            Some(canvas) => canvas,
-            None => return,
-        };
-
-        match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
-            WindowEvent::Resized(size) => state.resize(size.width, size.height),
-            // WindowEvent::ScaleFactorChanged {
-            //     scale_factor,
-            //     inner_size_writer,
-            // } => {
-            //     state.resize(width, height);
-            // }
-            WindowEvent::RedrawRequested => {
-                if state.paused {
-                    return;
-                }
-
-                state.update();
-                match state.render() {
-                    Ok(_) => {
-                        // Update frame count
-                        // self.dom.fps_counter.update();
-                    }
-                    // Reconfigure the surface if it's lost or outdated
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                        let size = state.window.inner_size();
-                        state.resize(size.width, size.height);
-                    }
-                    // The system is out of memory, we should probably quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => {
-                        log::error!("Out of memory while rendering!");
-                        event_loop.exit();
-                    }
-                    Err(e) => {
-                        log::error!("Unable to render {}", e);
-                    }
-                }
-            }
-            WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        physical_key: PhysicalKey::Code(code),
-                        state: key_state,
-                        ..
-                    },
-                ..
-            } => state.handle_key(event_loop, code, key_state.is_pressed()),
-            _ => {}
-        }
-    }
-}
-
-pub fn run() -> anyhow::Result<()> {
-    let event_loop = EventLoop::with_user_event().build()?;
-    let app = App::new(
-        #[cfg(target_arch = "wasm32")]
-        &event_loop,
-    );
-
-    #[cfg(target_arch = "wasm32")]
-    event_loop.spawn_app(app);
-
-    Ok(())
-}
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen(js_name = initRender)]
-pub fn init_render() -> Result<(), wasm_bindgen::JsValue> {
-    console_error_panic_hook::set_once();
-    run().unwrap_throw();
-
-    Ok(())
 }
