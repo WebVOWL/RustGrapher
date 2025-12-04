2e07b2fdd32aa09c0d1beb2d7c9c9cbf0854104e
nikarnik <nikarnik@live.dk>
2025-12-04 09:57:45 +0000

    move initial graph info out of State

--- src/web/app.rs
+++ src/web/app.rs
@@ -1,6 +1,9 @@
+use crate::web::prelude::NodeType;
+use crate::web::renderer::init_graph::InitGraph;
+
 use super::renderer::State;
 
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 #[cfg(target_arch = "wasm32")]
 use winit::platform::web::EventLoopExtWebSys;
@@ -55,6 +58,123 @@ impl ApplicationHandler<State> for App {
 
         let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
 
+        let mut positions = vec![
+            [50.0, 50.0],
+            [250.0, 50.0],
+            [450.0, 50.0],
+            [250.0, 250.0],
+            [650.0, 450.0],
+            [700.0, 50.0],
+            [850.0, 50.0],
+            [1050.0, 50.0],
+            [1050.0, 250.0],
+            [450.0, 250.0],
+            [650.0, 250.0],
+            [850.0, 250.0],
+            [850.0, 450.0],
+            [1250.0, 250.0],
+            [50.0, 500.0],
+            [1250.0, 150.0],
+            [1250.0, 350.0],
+            [150.0, 150.0],
+            [950.0, 100.0],
+            [350.0, 50.0],
+            [950.0, 25.0],
+            [950.0, 50.0],
+            [550.0, 450.0],
+            [575.0, 50.0],
+            [0.0, 0.0],
+            [0.0, 0.0],
+        ];
+        let mut labels = vec![
+            String::from("My class"),
+            String::from("Rdfs class"),
+            String::from("Rdfs resource"),
+            String::from("Loooooooong class 1 2 3 4 5 6 7 8 9"),
+            String::from("Thing"),
+            String::from("Eq1\nEq2\nEq3"),
+            String::from("Deprecated"),
+            String::new(),
+            String::from("Literal"),
+            String::new(),
+            String::from("DisjointUnion 1 2 3 4 5 6 7 8 9"),
+            String::new(),
+            String::new(),
+            String::from("This Datatype is very long"),
+            String::from("AllValues"),
+            String::from("Property1"),
+            String::from("Property2"),
+            String::new(),
+            String::new(),
+            String::from("is a"),
+            String::from("Deprecated"),
+            String::from("External"),
+            String::from("Symmetric"),
+            String::from("Property\nInverseProperty"),
+            String::new(),
+            String::new(),
+        ];
+        let mut node_types = vec![
+            NodeType::Class,
+            NodeType::RdfsClass,
+            NodeType::RdfsResource,
+            NodeType::ExternalClass,
+            NodeType::Thing,
+            NodeType::EquivalentClass,
+            NodeType::DeprecatedClass,
+            NodeType::AnonymousClass,
+            NodeType::Literal,
+            NodeType::Complement,
+            NodeType::DisjointUnion,
+            NodeType::Intersection,
+            NodeType::Union,
+            NodeType::Datatype,
+            NodeType::ValuesFrom,
+            NodeType::DatatypeProperty,
+            NodeType::DatatypeProperty,
+            NodeType::SubclassOf,
+            NodeType::DisjointWith,
+            NodeType::RdfProperty,
+            NodeType::DeprecatedProperty,
+            NodeType::ExternalProperty,
+            NodeType::ObjectProperty,
+            NodeType::InverseProperty,
+            NodeType::NoDraw,
+            NodeType::NoDraw,
+        ];
+        let mut edges = vec![
+            [0, 14, 1],
+            [13, 15, 8],
+            [8, 16, 13],
+            [0, 17, 3],
+            [9, 18, 12],
+            [1, 19, 2],
+            [10, 24, 11],
+            [11, 25, 12],
+            [6, 20, 7],
+            [6, 21, 7],
+            [4, 22, 4],
+            [2, 23, 5],
+            [5, 23, 2],
+        ];
+        let cardinalities: Vec<(u32, (String, Option<String>))> = vec![
+            (0, ("∀".to_string(), None)),
+            (8, ("1".to_string(), None)),
+            (1, ("1".to_string(), Some("10".to_string()))),
+            (10, ("5".to_string(), Some("10".to_string()))),
+        ];
+        let mut characteristics = HashMap::new();
+        characteristics.insert(21, "transitive".to_string());
+        characteristics.insert(23, "functional\ninverse functional".to_string());
+        let graph = InitGraph {
+            positions,
+            labels,
+            edges,
+            node_types,
+            cardinalities,
+            characteristics,
+        };
+
         #[cfg(target_arch = "wasm32")]
         {
             // Run the future asynchronously and use the
@@ -64,7 +184,7 @@ impl ApplicationHandler<State> for App {
                     assert!(
                         proxy
                             .send_event(
-                                State::new(window)
+                                State::new(window, graph)
                                     .await
                                     .expect("Unable to create canvas!!!")
                             )
